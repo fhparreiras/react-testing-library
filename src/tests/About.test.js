a6aa197f920d93e8ad6a16ae92ba46619da48fdb
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-02-18 17:28:47 -0300

    Requisito 2 parte 1 ok

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,24 @@
-test('', () => {});
-//
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes do componente ABOUT', () => {
+  test('a página deve conter as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const arrayTextA = ['This application simulates a Pokédex',
+      ' a digital encyclopedia containing all Pokémons'];
+    const textA = [...arrayTextA];
+
+    const arrayTextB = ['One can filter Pokémons by type',
+      ' and see more details for each one of them'];
+    const textB = [...arrayTextB];
+
+    const paragraphOne = screen.getByText(textA);
+    const paragraphTwo = screen.getByText(textB);
+
+    expect(paragraphOne).toBeVisible();
+    expect(paragraphTwo).toBeVisible();
+  });
+});
