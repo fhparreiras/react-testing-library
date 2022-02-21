41cd9f2c8cf5c4e72760e25b876b499ee738bdc3
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-02-21 15:55:13 -0300

    Parte 1 do requisito 5 ok

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes do componente POKEDEX', () => {
+  test('A página deve possuir um h2 com o texto "Encountered pokémons" ', () => {
+    renderWithRouter(<App />);
+
+    const head = screen.getByRole('heading', { name: /Encountered pokémons/i, level: 2 });
+    expect(head).toBeDefined();
+  });
+});
