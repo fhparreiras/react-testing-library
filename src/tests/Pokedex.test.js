aefdc7a40e384b1a1b2a8b2816ce6f1565f06044
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-02-21 16:42:55 -0300

    Requisito 5 parte 2 ok

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { getByRole, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
@@ -9,5 +10,25 @@ describe('Testes do componente POKEDEX', () => {
 
     const head = screen.getByRole('heading', { name: /Encountered pokémons/i, level: 2 });
     expect(head).toBeDefined();
+  });
+
+  test('Deve exibir o Pokémon da lista ao clicar no botão "Próximo pokémon" ', () => {
+    renderWithRouter(<App />);
+    const btnNext = screen.getByRole('button', { name: /próximo pokémon/i });
+
+    userEvent.click(btnNext);
+    const charmanderText = screen.getByText(/charmander/i);
+    expect(charmanderText).toBeDefined();
+
+    const clicks = 7;
+    for (let i = 1; i <= clicks; i += 1) {
+      userEvent.click(btnNext);
+    }
+    const dragonairText = screen.getByText(/dragonair/i);
+    expect(dragonairText).toBeDefined();
+
+    userEvent.click(btnNext);
+    const pikachuText = screen.getByText(/pikachu/i);
+    expect(pikachuText).toBeDefined();
   });
 });
