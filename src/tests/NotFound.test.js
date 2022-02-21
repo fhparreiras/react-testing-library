6605769d8e6f28007506ab80930dbb32df39ca77
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-02-21 15:24:17 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes do componente NOTFOUND', () => {
+  test('A página deve conter um h2 com o texto "Page requested not found 😭" ', () => {
+    renderWithRouter(<NotFound />);
+    const heading = screen.getByRole('heading', { level: 2 });
+    const headingText = screen.getByText(/Page requested not found/i);
+    const headingEmoji = screen.getByText('😭');
+
+    expect(heading).toBeDefined();
+    expect(headingText).toBeDefined();
+    expect(headingEmoji).toBeDefined();
+  });
+
+  test('A página deve possuir a imagem com uma source específica', () => {
+    renderWithRouter(<NotFound />);
+
+    const imgNotFound = screen.getByRole('img',
+      { name: /pikachu crying because the page requested was not found/i });
+    const imgSource = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(imgNotFound).toHaveAttribute('src', imgSource);
+  });
+});
