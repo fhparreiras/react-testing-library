69dbf4c6ccb79ee975cf4aa7d9d44768b2e2ac87
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-02-21 19:25:01 -0300

    Requisito 6 parte 1

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes do componente POKEMON', () => {
+  test('O card do pokémon deve ser renderizado corretamente ', () => {
+    renderWithRouter(<App />);
+    const pikachuText = screen.getByText(/pikachu/i);
+    const pikachuType = screen.getAllByText(/electric/i);
+    const value = '6.0';
+    const measurementUnit = 'kg';
+    const pikachuInfo = `Average weight: ${value} ${measurementUnit}`;
+    const imgSource = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
+    const imagePikachu = screen.getByRole('img', { name: /pikachu sprite/i });
+    expect(pikachuText).toBeDefined();
+    expect(pikachuType[0]).toBeDefined();
+    expect(pikachuInfo).toBeDefined();
+    expect(imagePikachu).toHaveAttribute('src', imgSource);
+    expect(imagePikachu).toHaveAttribute('alt', 'Pikachu sprite');
+  });
+});
