85052291ab72b24c619488e802034de70e305b68
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-02-21 19:12:13 -0300

    Ajuste lint

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { getByRole, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import filterPokemons from '../components/Pokedex';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
